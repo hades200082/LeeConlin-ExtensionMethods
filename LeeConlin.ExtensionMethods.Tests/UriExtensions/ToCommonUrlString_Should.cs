edf4f093b9a70cebe4caf5f201d290fa5a2b5d76
Lee Conlin <lee@wildkatz.org>
2020-07-08 11:35:16 +0100

    Added extensions for .ToCommonUrlString to ignore known ports
    http = 80
    https = 443
    ftp = 21

---
+++ LeeConlin.ExtensionMethods.Tests/UriExtensions/ToCommonUrlString_Should.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace LeeConlin.ExtensionMethods.Tests.UriExtensions
+{
+    public class ToCommonUrlString_Should
+    {
+        [Theory]
+        [InlineData("https://www.google.co.uk", "https://www.google.co.uk/")]
+        [InlineData("http://www.google.co.uk", "http://www.google.co.uk/")]
+        [InlineData("https://www.google.co.uk:44353", "https://www.google.co.uk:44353/")]
+        [InlineData("http://www.google.co.uk:8080", "http://www.google.co.uk:8080/")]
+        public void CorrectlyRemovePortNumbersForKnownSchemes(string testUrl, string expected)
+        {
+            var sut = new Uri(testUrl);
+
+            sut.ToCommonUrlString().Should().Be(expected);
+        }
+    }
+}
